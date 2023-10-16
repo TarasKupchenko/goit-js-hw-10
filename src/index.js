70fd615d5c59626d318880ae40e09e6b28a945c5
TarasKupchenko <133959505+TarasKupchenko@users.noreply.github.com>
2023-10-16 20:00:44 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,62 +1,20 @@
-
-//import SlimSelect from 'slim-select';
-//import Notiflix from "notiflix";
-//import { fetchBreeds, fetchCatByBreed } from "./cat-api.js";
-
-//const breedSelect = document.querySelector(".breed-select");
-//const catInfo = document.querySelector(".cat-info");
-//const loader = document.querySelector(".loader");
-//const error = document.querySelector(".error");
-
-//fetchBreeds()
-//  .then((response) => {
-//    const breeds = response.data;
-//    breeds.forEach((breed) => {
-//      const option = document.createElement("option");
-//      option.value = breed.id;
-//      option.textContent = breed.name;
-//      breedSelect.appendChild(option);
-//    });
-//  })
-//  .catch((error) => {
-//    console.error("Помилка при завантаженні списку порід", error);
-//  });
-
-//breedSelect.addEventListener("change", () => {
-//  const selectedBreedId = breedSelect.value;
-//  loader.style.display = "block"; 
-//  error.style.display = "none";
-//  fetchCatByBreed(selectedBreedId)
-//    .then((response) => {
-     
-//      const catData = response.data[0]; 
-//      catInfo.innerHTML = `<img src="${catData.url}" alt="Cat Image">
-//                           <div style="max-width: 500px">
-//                           <p style="font-size: 32px; color: blue;">${catData.breeds[0].name}</p>
-//                           <p> <span style="font-weight: 700;">Description: </span>${catData.breeds[0].description}</p>
-//                           <p><span style="font-weight: 700;">Temperament: </span>${catData.breeds[0].temperament}</p>
-//                           </div>`;
-//    })
-//    .catch((error) => {
-//      console.error("Помилка при запиті інформації про кота", error);
-//      error.style.display = "block"; 
-//    })
-//    .finally(() => {
-//      loader.style.display = "none"; 
-//    });
-//});
-import Notiflix from 'notiflix';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const breedSelect = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
 
 // Показуємо лоадер при завантаженні сторінки
 loader.style.display = 'block';
 
 // Приховуємо селект при завантаженні сторінки
 breedSelect.style.display = 'none';
+
+function showError(errorMessage) {
+  error.textContent = errorMessage;
+  error.style.display = 'block';
+}
 
 // Функція для приховування блоку з інформацією про породу
 function hideCatInfo() {
@@ -79,7 +37,8 @@ fetchBreeds()
   })
   .catch((error) => {
     console.error('Помилка при завантаженні списку порід', error);
-    Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
+    // Виводимо повідомлення про помилку на сторінці
+    showError('Oops! Something went wrong! Try reloading the page!');
     // Приховуємо лоадер у випадку помилки
     loader.style.display = 'none';
   });
@@ -93,19 +52,19 @@ breedSelect.addEventListener('change', () => {
   fetchCatByBreed(selectedBreedId)
     .then((response) => {
       const catData = response.data[0];
-      catInfo.innerHTML = `
-        <img src="${catData.url}" alt="Cat Image">
-        <div style="max-width: 500px">
-        <p style="font-size: 32px; color: blue;">${catData.breeds[0].name}</p>
-        <p> <span style="font-weight: 700;">Description: </span>${catData.breeds[0].description}</p>
-        <p><span style="font-weight: 700;">Temperament: </span>${catData.breeds[0].temperament}</p>
-        </div>`;
+      catInfo.innerHTML = `<img src="${catData.url}" alt="Cat Image">
+                                <div style="max-width: 500px">
+                                <p style="font-size: 32px; color: blue;">${catData.breeds[0].name}</p>
+                                <p> <span style="font-weight: 700;">Description: </span>${catData.breeds[0].description}</p>
+                               <p><span style="font-weight: 700;">Temperament: </span>${catData.breeds[0].temperament}</p>
+                                </div>`;
       // Показуємо блок інформації про нову породу
-      catInfo.style.display = 'block';
+      catInfo.style.display = 'flex';
     })
     .catch((error) => {
       console.error('Помилка при запиті інформації про кота', error);
-      Notiflix.Notify.Failure('Oops! Something went wrong! Try reloading the page!');
+      showError('Oops! The selected breed was not found. Please try again.');
+      hideCatInfo();
     })
     .finally(() => {
       loader.style.display = 'none';
